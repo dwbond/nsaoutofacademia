c70c0e8e4014d526754e9cbc93d2153e119457cd
Daniel W Bond <dbond2@gmu.edu>
2014-02-03 20:46:26 -0500

    added to models and settings

--- nsaoutofacademia/website/models.py
+++ nsaoutofacademia/website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length = 150)
+    title = models.CharField(max_length = 150)
+    email = models.EmailField()
+
+class University(models.Model):
+    universityName = models.CharField(max_length = 150)
+    centerName = models.CharField(max_length = 150)
+    page = models.URLField()
+    people = models.ManyToManyField('Person')  
