717587134cc4585120384eccdeeb8393edeb5bf5
Daniel W Bond <dbond2@gmu.edu>
2014-02-03 21:39:25 -0500

    changed a field in models

--- nsaoutofacademia/website/models.py
+++ nsaoutofacademia/website/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length = 150)
-    title = models.CharField(max_length = 150)
+    titleName = models.CharField(max_length = 150)
     email = models.EmailField()
 
 class University(models.Model):
     universityName = models.CharField(max_length = 150)
     centerName = models.CharField(max_length = 150)
+    centerEmail = models.EmailField()
     page = models.URLField()
     people = models.ManyToManyField('Person')  
