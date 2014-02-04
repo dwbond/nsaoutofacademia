e047fbae9600bade4c8053e825fd4554ca0a961c
Daniel W Bond <dbond2@gmu.edu>
2014-02-03 21:42:40 -0500

    registered the admin

--- nsaoutofacademia/website/admin.py
+++ nsaoutofacademia/website/admin.py
@@ -1,3 +1,9 @@
+from django.db import models
 from django.contrib import admin
 
-# Register your models here.
+from django.forms import CheckBoxSelectMultiple
+
+from website.models import Person, University
+
+admin.site.register(Person)
+admin.site.register(Seller)
