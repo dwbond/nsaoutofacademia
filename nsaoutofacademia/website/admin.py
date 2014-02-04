bffe93f2005e98cdd286a7d75589e829916b988b
Daniel W Bond <dbond2@gmu.edu>
2014-02-03 21:47:36 -0500

    fixes import error

--- nsaoutofacademia/website/admin.py
+++ nsaoutofacademia/website/admin.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib import admin
-
-from django.forms import CheckBoxSelectMultiple
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from django.forms import CheckboxSelectMultiple
 
 from website.models import Person, University
 
 admin.site.register(Person)
-admin.site.register(Seller)
+admin.site.register(University)
